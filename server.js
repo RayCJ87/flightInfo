9ac51a6c78b99f3400bcf9606a8190b4e2c87d50
RayCJ87 <shihcr.ke@gmail.com>
2019-04-02 01:24:08 -0400

    index view pages added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
@@ -7,19 +8,26 @@ const fetch = require("node-fetch");
 
 dotenv.config();
 
+app.set("view engine", "ejs")
+
+
 let profile;
 
 
 app.get("/", (req, res) =>{
-    fetch(`http://aviation-edge.com/v2/public/timetable?key=${process.env.API_KEY}&iataCode=YYZ&type=departure`)
+    fetch(`http://aviation-edge.com/v2/public/timetable?key=${process.env.API_KEY2}&iataCode=YYZ&type=departure`)
     .then(res => res.json())
     .then(json => profile = json)
     res.send(profile);
 
 })
 
+app.get("/home", (req, res)=> {
+    res.render("index.ejs");
+})
+
 app.listen(PORT, () => {
     console.log(`The test app listening on port ${PORT}!`);
     console.log(`the api url here: ${process.env.API_URL}`);
-    console.log(`The api key is : ${process.env.API_KEY}`);
+    console.log(`The api key is : ${process.env.API_KEY2}`);
 })
