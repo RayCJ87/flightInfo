07564719c788d49918ac786b918c1e10a593feff
RayCJ87 <shihcr.ke@gmail.com>
2019-04-02 16:45:36 -0400

    update search page

--- server.js
+++ server.js
@@ -11,21 +11,32 @@ dotenv.config();
 app.set("view engine", "ejs")
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 let profile;
 
 
 app.get("/", (req, res) =>{
-    fetch(`http://aviation-edge.com/v2/public/timetable?key=${process.env.API_KEY2}&iataCode=YYZ&type=departure`)
-    .then(res => res.json())
-    .then(json => profile = json)
-    res.send(profile);
 
 })
 
 app.get("/home", (req, res)=> {
     
     res.render("index.ejs");
+})
+
+app.get("/result", (req, res)=> {
+    const airName = req.body.flightC;
+    const flightNumber = req.body.flightNum;
+    console.log(req.body);
+    console.log(`the airline is: ${airName}`)
+    console.log(`the flightnumber is: ${flightNumber}`)
+    fetch(`http://aviation-edge.com/v2/public/timetable?key=${process.env.API_KEY2}&iataCode=YYZ&type=departure`)
+    .then(res => res.json())
+    .then(json => profile = json)
+
+    res.render("result.ejs")
+
 })
 
 app.listen(PORT, () => {
